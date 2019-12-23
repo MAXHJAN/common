0b08d0c1c2072a8642cdb618d613a4b0d10d94c5
Mx <hjmaxiao@gmail.com>
2019-12-23 14:09:48 +0800

    common

---
+++ src/main/java/com/mx/common/ResultCode.java
@@ -0,0 +1,31 @@
+package com.mx.common;
+
+/**
+ * 枚举了一些常用API操作码
+ * Created by macro on 2019/4/19.
+ */
+public enum ResultCode implements IErrorCode {
+    SUCCESS(200, "操作成功"),
+    FAILED(500, "操作失败"),
+    VALIDATE_FAILED(400, "参数检验失败"),
+    UNAUTHORIZED(401, "暂未登录或token已经过期"),
+    FORBIDDEN(403, "没有相关权限"),
+    USER_NAME_REPEAT(4031, "用户名已存在"),
+    USER_PHONE_REPEAT(4032, "电话号已存在"),
+    USER_NOT_FOUND(4004, "用户不存在");
+    private int code;
+    private String message;
+
+     ResultCode(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
